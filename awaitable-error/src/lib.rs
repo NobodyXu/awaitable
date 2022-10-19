f2dbc11a08f0d8b14e44f9203f27fc40a7e3156d
Jiahao XU <Jiahao_XU@outlook.com>
2022-10-19 19:07:11 +1100

    Optimize `awaitable-error`: Rm dep `thiserror`
    Impls `std::error::Error` and `std::fmt::Display` manually
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- awaitable-error/src/lib.rs
+++ awaitable-error/src/lib.rs
@@ -1,14 +1,27 @@
-#[derive(Debug, thiserror::Error)]
+use std::{error, fmt};
+
+#[derive(Debug)]
 pub enum Error {
     /// Awaitable is not initialized yet.
-    #[error("Awaitable is not initialized yet.")]
     Uninitialized,
 
     /// Awaitable is already consumed but not yet reset.
-    #[error("Awaitable is already consumed but not yet reset.")]
     AlreadyConsumed,
 
     /// Awaitable is marked done twice.
-    #[error("Awaitable is marked done twice.")]
     AlreadyDone,
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Error::*;
+
+        f.write_str(match self {
+            Uninitialized => "Awaitable is not initialized yet.",
+            AlreadyConsumed => "Awaitable is already consumed but not yet reset.",
+            AlreadyDone => "Awaitable is marked done twice.",
+        })
+    }
+}
+
+impl error::Error for Error {}
